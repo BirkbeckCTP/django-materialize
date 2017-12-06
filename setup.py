bcfbfd9150887328dddce965674c84b2b6e6b785
Andy Byers <a.byers@bbk.ac.uk>
2017-12-06 17:20:28 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,6 @@ try:
 except ImportError:
     from distutils.core import setup
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-
 setup(
     name='django-materialize',
     version=1.0,
