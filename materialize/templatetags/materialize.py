baace20177e8b0c9816afa52e07260aca1ebb5f7
Andy Byers <andy.byers@openlibhums.org>
2019-04-24 11:21:45 +0100

    Fix for django 2.x

--- materialize/templatetags/materialize.py
+++ materialize/templatetags/materialize.py
@@ -4,15 +4,18 @@ from django.template import loader
 from django import template
 register = template.Library()
 
-@register.assignment_tag
+
+@register.simple_tag()
 def get_language():
     from django.conf import settings
     language = settings.LANGUAGE_CODE.split('-')
     return language[0]
 
+
 @register.simple_tag()
 def materialize_form(*args, **kwargs):
     return render_form(*args, **kwargs)
+
 
 def render_form(form, form_title='', data_success='', data_error=''):
     t = loader.get_template('forms/simple_form.html')
